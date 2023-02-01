27997d039eebe93bb0124ae945f447414273c0f6
unknown <nae.imuam103@gmail.com>
2023-02-02 01:36:04 +0530

    cover JSX, rendering elements, components and props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Welcome extends React.Component{
+    render(){
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+function formatName(user){
+    return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user){
+    if (user) {
+        return (
+            <div>
+                <Welcome name={formatName(user)} />
+                <Welcome name="Eram Mansoor" />
+            </div>
+        );
+    }
+    return <h1>Hello, Stranger.</h1>
+}
+
+const user = {
+    firstName: 'Nae',
+    lastName: 'ImUam'
+};
+function tick(){
+    
+    const element = (
+        <div>
+            <h1>{getGreeting(user)}</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    root.render(element);
+}
+
+setInterval(tick, 1000);
